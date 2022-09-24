b51104259411f59d1cdeb27c5da2c46448c6e212
winechitpaing <winechitpaing@codigo.co>
2022-09-24 11:49:52 +0630

    chore: set up view binding and onclick listener

--- app/src/main/java/com/llc/todolist/MainActivity.kt
+++ app/src/main/java/com/llc/todolist/MainActivity.kt
@@ -2,10 +2,34 @@ package com.llc.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.llc.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.textView.text = "Hello World"
+        val tx = binding.textView.text.toString()
+
+        binding.edt.setText("Happy Birthday")
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this, tx, Toast.LENGTH_LONG).show()
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            val editText = binding.edt.text.toString()
+            Toast.makeText(this, editText, Toast.LENGTH_LONG).show()
+        }
     }
 }
+
+// https://developer.android.com/topic/libraries/view-binding#kotlin
