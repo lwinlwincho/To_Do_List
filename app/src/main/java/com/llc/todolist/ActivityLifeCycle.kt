981276f13e5e527755f51e867428b8c7ec09f8da
lwincho-dev <paingdev@gmail.com>
2022-09-30 10:46:28 +0630

    explore: activity life cycle

---
+++ app/src/main/java/com/llc/todolist/ActivityLifeCycle.kt
@@ -0,0 +1,55 @@
+package com.llc.todolist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.llc.todolist.databinding.ActivityLifeCycleBinding
+
+class ActivityLifeCycle : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLifeCycleBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLifeCycleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.d(TAG, "onCreate Called")
+
+        binding.textView.text = TAG
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+}
